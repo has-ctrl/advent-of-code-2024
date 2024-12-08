a444d0128c6e240ab60c5fa69889c3c5dbdbd4f5
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2024-12-08 21:15:06 +0100

    Day 08 - Part 2

--- days/08.py
+++ days/08.py
@@ -13,22 +13,39 @@ for j, row in enumerate(raw_data):
             data[char] += [(i, j)]
 
 
-def one() -> int:
+def _is_valid(x: int, y: int) -> bool:
+    return 0 <= x < MAX_X and 0 <= y < MAX_Y
+
+
+def one(boost: bool = False) -> int:
     """
+    Calculate the impact of the signal. How many unique locations within the bounds of the map contain an antinode?
     """
     antinodes = set()
     for _, coords in data.items():
         for (x1, y1), (x2, y2) in combinations(coords, 2):
+            if boost:
+                antinodes.add((x1, y1))
+                antinodes.add((x2, y2))
             x_distance, y_distance = x1 - x2, y1 - y2
-            antinodes.add((x1 + x_distance, y1 + y_distance))
-            antinodes.add((x2 - x_distance, y2 - y_distance))
-    return len([(x, y) for x, y in antinodes if 0 <= x < MAX_X and 0 <= y < MAX_Y])
+            while True:
+                x1, y1 = x1 + x_distance, y1 + y_distance
+                x2, y2 = x2 - x_distance, y2 - y_distance
+                if val1 := _is_valid(x1, y1):
+                    antinodes.add((x1, y1))
+                if val2 := _is_valid(x2, y2):
+                    antinodes.add((x2, y2))
+                if not boost or (not val1 and not val2):
+                    break
+    return len(antinodes)
 
 
 def two() -> int:
     """
+    Calculate the impact of the signal using this updated model. How many unique locations within the bounds of the map
+    contain an antinode?
     """
-    return 0
+    return one(boost=True)
 
 
 print(f"1. {one()}")
