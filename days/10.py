997e639603852766c429cb01fb06357d1182ee81
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2024-12-10 21:45:02 +0100

    Day 10 - Part 2

--- days/10.py
+++ days/10.py
@@ -11,38 +11,43 @@ for j, row in enumerate(data):
             trailheads.append((i, j))
 
 
-def one() -> int:
+def one(track_distinct_paths: bool = False) -> int:
     """
     The reindeer gleefully carries over a protractor and adds it to the pile. What is the sum of the scores of all
     trailheads on your topographic map?
     """
     count = 0
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     for start in trailheads:
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         visited = set()
         queue = [start]
+        distinct_paths = {start: 1}
         visited.add(start)
 
         while queue:
             x, y = queue.pop(0)
 
             if data[y][x] == 9:
-                count += 1
+                count += 1 if not track_distinct_paths else distinct_paths[(x, y)]
                 continue
 
             for d_x, d_y in directions:
                 new = new_x, new_y = x + d_x, y + d_y
-                if 0 <= new_x < MAX_X and 0 <= new_y < MAX_Y and new not in visited:
+                if 0 <= new_x < MAX_X and 0 <= new_y < MAX_Y:
                     if data[new_y][new_x] == data[y][x] + 1:
-                        queue.append(new)
-                        visited.add(new)
+                        if new not in visited:
+                            queue.append(new)
+                            visited.add(new)
+                        distinct_paths[(new_x, new_y)] = distinct_paths.get((new_x, new_y), 0) + distinct_paths.get((x, y), 0)
     return count
 
 
 def two() -> int:
     """
+    You're not sure how, but the reindeer seems to have crafted some tiny flags out of toothpicks and bits of paper and
+    is using them to mark trailheads on your topographic map. What is the sum of the ratings of all trailheads?
     """
-    return 0
+    return one(track_distinct_paths=True)
 
 
 print(f"1. {one()}")
