6a3683c6411383c053809cf7f49d76e038450b4a
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2024-12-14 14:52:17 +0100

    Day 12 - Part One

--- days/12.py
+++ days/12.py
@@ -1,8 +1,9 @@
-from collections import defaultdict
+from collections import deque
 
 from get_day_input import get_input
 
 data = get_input(day=12).splitlines()
+
 
 MAX_Y, MAX_X = len(data), len(data[0])
 
@@ -12,24 +13,29 @@ def one() -> int:
     What is the total price of fencing all regions on your map?
     """
     total = 0
-    area_dict, perimeter_dict, expected_dict, visited_dict = {}, {}, defaultdict(set), defaultdict(set)
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    for y, line in enumerate(data):
-        for x, plant in enumerate(line):
-            visited_dict[plant].add((x, y))
-            expected_dict[plant].add((x, y))
-            area_dict[plant] = area_dict.get(plant, 0) + 1
-            for d_x, d_y in directions:
-                new_x, new_y = x + d_x, y + d_y
-                if 0 <= new_x < MAX_X and 0 <= new_y < MAX_Y:
-                    if data[new_y][new_x] == plant:
-                        expected_dict[plant].add((new_x, new_y))
-                        continue
-                perimeter_dict[plant] = perimeter_dict.get(plant, 0) + 1
-
-            if expected_dict[plant] == visited_dict[plant]:
-                total += area_dict[plant] * perimeter_dict.get(plant, 0)
-                area_dict[plant], perimeter_dict[plant] = 0, 0
+    queue = deque()
+    visited = set()
+    for j, line in enumerate(data):
+        for i, plant in enumerate(line):
+            if (i, j) in visited:
+                continue
+            visited.add((i, j))
+            queue.appendleft((i, j))
+            area = perimeter = 0
+            while queue:
+                x, y = queue.popleft()
+                area += 1
+                for d_x, d_y in directions:
+                    adj_coords = new_x, new_y = x + d_x, y + d_y
+                    if 0 <= new_x < MAX_X and 0 <= new_y < MAX_Y:
+                        if data[new_y][new_x] == plant:
+                            if adj_coords not in visited:
+                                visited.add(adj_coords)
+                                queue.append(adj_coords)
+                            continue
+                    perimeter += 1
+            total += area * perimeter
     return total
 
 
