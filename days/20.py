3f3a9a974c459a2b43bd1aedb91f101d461711cd
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2025-01-05 16:35:05 +0100

    Day 20 - Part 2

--- days/20.py
+++ days/20.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from itertools import combinations
 from get_day_input import get_input
 
 DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
@@ -17,52 +17,48 @@ def _get_start() -> tuple[tuple[int, int], tuple[int, int]]:
     return s, e
 
 
-def _calc_max_picoseconds() -> tuple[dict[tuple, int], int]:
-    picoseconds = 0
+def _calc_max_picoseconds() -> dict[tuple, int]:
     start, end = _get_start()
     visited = {start}
-    path = {start: picoseconds}
+    path = {start: 0}
     x, y = start
     while True:
         if (x, y) == end:
             break
-        picoseconds += 1
         for d_x, d_y in DIRECTIONS:
             new = new_x, new_y = x + d_x, y + d_y
             if new not in visited and data[new_y][new_x] != "#":
-                path[new] = picoseconds
+                path[new] = path[(x, y)] + 1
                 visited.add(new)
                 x, y = new
                 break
-    return path, picoseconds
+    return path
 
 
-def _calc_picoseconds_saved(path: dict, track_length: int, min_saved: int = 100) -> int:
+def _calc_picoseconds_saved(path: dict,max_time: int = 20, min_saved: int = 100) -> int:
     count = 0
-    for x, y in path:
-        for d_x, d_y in DIRECTIONS:
-            c_x, c_y = x + d_x, y + d_y
-            cheat_end = x + d_x * 2, y + d_y * 2
-            if cheat_end in path and data[c_y][c_x] == "#":
-                if path[(x, y)] + track_length - path[cheat_end] + 2 <= track_length - min_saved:
-                    count += 1
+    for ((start_x, start_y), start_dis), ((end_x, end_y), end_dis) in combinations(path.items(), 2):
+        distance = abs(start_x - end_x) + abs(start_y - end_y)
+        if end_dis - start_dis - distance >= min_saved:
+            if distance <= max_time:
+                count += 1
     return count
 
 
-def one() -> int:
+def one(max_time: int = 2) -> int:
     """
     You aren't sure what the conditions of the racetrack will be like, so to give yourself as many options as possible,
     you'll need a list of the best cheats. How many cheats would save you at least 100 picoseconds?
     """
-    path, picoseconds = _calc_max_picoseconds()
-    return _calc_picoseconds_saved(path, picoseconds)
+    path = _calc_max_picoseconds()
+    return _calc_picoseconds_saved(path, max_time)
 
 
 def two() -> int:
     """
     Find the best cheats using the updated cheating rules. How many cheats would save you at least 100 picoseconds?
     """
-    return 0
+    return one(max_time=20)
 
 
 print(f"1. {one()}")
